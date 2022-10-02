ecdc58b316d8ee3ac4ce0cadddefe708a42ed197
iia.m.y <frezhgrand@gmail.com>
2022-10-02 18:38:06 +0300

    setup project configuration

--- src/main/java/by/bntb/priceanalyst/PriceAnalystApplication.java
+++ src/main/java/by/bntb/priceanalyst/PriceAnalystApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PriceAnalystApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PriceAnalystApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PriceAnalystApplication.class, args);
+    }
 
 }
