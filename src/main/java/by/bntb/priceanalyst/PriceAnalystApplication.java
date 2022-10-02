123cd39949771af91b5d7f2627c2c80b8e1b10d4
iia.m.y <frezhgrand@gmail.com>
2022-10-02 18:06:27 +0300

    initial commit

---
+++ src/main/java/by/bntb/priceanalyst/PriceAnalystApplication.java
@@ -0,0 +1,13 @@
+package by.bntb.priceanalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceAnalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceAnalystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bntb/priceanalyst/PriceAnalystApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bntb.priceanalyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAnalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
